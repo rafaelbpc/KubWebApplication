06a85a77e589b794514abe12c148f4bf9ac58c89
Rafael Lino <rafaels@bompracredito.com.br>
2020-02-07 16:30:45 -0300

    commit

---
+++ KubWebApplication/Controllers/WeatherForecastController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+
+namespace KubWebApplication.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { Environment.MachineName, DateTime.Now.ToString() };
+        }
+
+    }
+}
